20de0b054730e92a7635926d47f42e5a2816386d
akinyiliz <lizzieke84@gmail.com>
2023-07-17 18:54:25 +0300

    Main file, routes defined

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import "package:countries_app/pages/home.dart";
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MaterialApp(
+    // home: Home(),
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const Home(),
+    },
+  ));
+}
